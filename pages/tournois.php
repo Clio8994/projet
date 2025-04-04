8da7c60542dca57758623530cf9a1e25998f99e8
mwilson944 <wilsonmai85@gmail.com>
2025-04-04 18:19:24 +0200

    maj tournois

---
+++ pages/tournois.php
@@ -0,0 +1,13 @@
+<?php include '../inc/header.php'; ?>
+<?php include '../inc/nav.php'; ?>
+<main>
+    <h1>Bienvenue sur notre site de tournois de Bowling</h1>
+    <p>
+    <h2>Tournois à venir !<h2>
+    <ul>
+                    <li>Tournoi régional - 15 avril 2025</li>
+                    <li>Championnat national - 22 mai 2025</li>
+                    <li>Open international - 10 juin 2025</li>
+    </ul>
+</main>
+<?php include '../inc/footer.php'; ?>
