4dca8911e980cc26981528733b96b9b6b857513f
Clio8994 <elio.hucko@gmail.com>
2025-04-04 16:43:10 +0200

    ajout nav

---
+++ inc/nav.php
@@ -0,0 +1,11 @@
+<?php 
+define ('BASE', '/projet/');
+?>
+
+<body>
+    <nav>
+        <a href="<?= BASE ?>">Accueil</a>
+        <a href="<?= BASE ?>pages/tournois.php">Tournois</a>
+        <a href="<?= BASE ?>pages/classement.php">Classement</a>
+        <a href="<?= BASE ?>pages/admin.php">Admin</a>
+    </nav>
