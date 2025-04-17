216383c5f3f74f759a2b95ebae45e5bcd02fbdfd
Clio8994 <elio.hucko@gmail.com>
2025-04-17 09:49:05 +0200

    modification tournois.php

--- pages/tournois.php
+++ pages/tournois.php
@@ -1,35 +1,37 @@
 <<?php
-include '../inc/header.php';
-include '../inc/nav.php';
-include '../inc/key.php';
-?>
-<main>
-        <h1>Répartition des prix</h1>
-        <h2>1er</h2>
-        <h2>2ème</h2>
-        <h3>3ème</h3>
-        <article>
-            Format du tournois : 
+    include '../inc/header.php';
+    include '../inc/nav.php';
+    include '../inc/key.php';
+    ?>
+    <main>
+    <h1>Répartition des prix</h1>
+    <h2>1er</h2>
+    <h2>2ème</h2>
+    <h3>3ème</h3>
+    <article>
+        <p>
+            Format du tournois :
             Chaque équipe joue 2 parties lors de la phase de qualification.
 
-Les 4 meilleures équipes score cumulé accèdent à la finale.
+            Les 4 meilleures équipes score cumulé accèdent à la finale.
 
-En finale, chaque joueur joue 1 partie : le score total de l'équipe détermine le classement final.
+            En finale, chaque joueur joue 1 partie : le score total de l'équipe détermine le classement final.
 
-En cas d'égalité : départage avec une frame décisive par capitaine d'équipe.
+            En cas d'égalité : départage avec une frame décisive par capitaine d'équipe.
 
-Matériel fourni sur place boules et chaussures.
+            Matériel fourni sur place boules et chaussures.
+        </p>
 
 
-        </article>
-       <section>
+    </article>
+    <section>
         Adresse :
         Dates :
-        Horaires : 
+        Horaires :
         Frais d'inscription :
-        Date limite d'inscription : 
+        Date limite d'inscription :
 
-       </section> 
+    </section>
     </main>
     <?php include '../inc/form.php'; ?>
-        <?php include '../inc/footer.php'; ?>
+    <?php include '../inc/footer.php'; ?>
