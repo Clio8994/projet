417ae9339ebbd4d3b6b8b09cd0b227508d4812ed
mwilson944 <wilsonmai85@gmail.com>
2025-04-17 10:17:36 +0200

    modif tournois

--- pages/tournois.php
+++ pages/tournois.php
@@ -3,34 +3,28 @@
     include '../inc/nav.php';
     ?>
     <main>
-    <h1>Répartition des prix</h1>
-    <h2>1er</h2>
-    <h2>2ème</h2>
-    <h3>3ème</h3>
-    <article>
-        <p>
-            Format du tournois :
-            Chaque équipe joue 2 parties lors de la phase de qualification.
-
-            Les 4 meilleures équipes score cumulé accèdent à la finale.
-
-            En finale, chaque joueur joue 1 partie : le score total de l'équipe détermine le classement final.
-
-            En cas d'égalité : départage avec une frame décisive par capitaine d'équipe.
-
-            Matériel fourni sur place boules et chaussures.
-        </p>
-
-
-    </article>
-    <section>
-        Adresse :
-        Dates :
-        Horaires :
-        Frais d'inscription :
-        Date limite d'inscription :
-
-    </section>
+    <table border="1">
+        <thead>
+            <th>Nom</th>
+            <th>Date</th>
+            <th>Lieu</th>
+            <th>Description</th>
+        </thead>
+        <?php
+        include "../inc/key.php";
+        $sql = "SELECT * FROM tournois";
+        $book = $connexion->query($sql);
+        foreach ($book as $b):
+        ?>
+            <tbody>
+                <td><?= $b["Nom"]; ?></td>
+                <td><?= $b["Date"]; ?></td>
+                <td><?= $b["Lieu"]; ?></td>
+                <td><?= $b["Description"]; ?></td>
+            </tbody>
+        <?php endforeach; ?>
+    </table>
+    
     </main>
     <?php include '../inc/form.php'; ?>
     <?php include '../inc/footer.php'; ?>
